0efd701967cc3d9f6abc7fc764424c8184e9aea8
rebeccadrennan <rebeccadrennan@gmail.com>
2026-07-01 16:33:23 +0100

    Initial commit

---
+++ src/app/routes.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import withRouter from "../hooks/withRouter";
+import { Home } from "../pages/home";
+import { Portfolio } from "../pages/portfolio";
+import { ContactUs } from "../pages/contact";
+import { About } from "../pages/about";
+import { LinkedIn } from "../pages/linkedin";
+import { Socialicons } from "../components/socialicons";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+
+const AnimatedRoutes = withRouter(({ location }) => (
+  <div className="scroll-container">
+    <section id="home">
+      <Home />
+    </section>
+    <section id="contact">
+      {" "}
+      <ContactUs />{" "}
+    </section>
+    <section id="about">
+      <About />
+    </section>
+    <section id="portfolio">
+      <Portfolio />
+    </section>
+    <section id="linkedin">
+      <LinkedIn />
+    </section>
+
+    <Socialicons />
+  </div>
+));
+
+function AppRoutes() {
+  return (
+    <div className="s_c">
+      <AnimatedRoutes />
+      <Socialicons />
+    </div>
+  );
+}
+
+export default AppRoutes;
